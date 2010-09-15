51bd0be340a675a6c06f697968e6b8d904c8a493
= <=>
2010-09-14 22:36:17 -0500

    Turned into Rails3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boutique::Application.config.secret_token = '5e89fc1c4e1a64d7db421bca5bd81dd04d0e36f88fa88a37921b183ed0d9c85bec317c9185fe8f510f5bb62a44dafccda9f446fd586b6ea7d7bf11e777bcd9fa'
